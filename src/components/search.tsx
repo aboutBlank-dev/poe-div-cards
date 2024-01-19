b95d691d4c428654ce3c49bf55d61d15d415d588
aboutBlank-dev <josecolaco1999@gmail.com>
2024-01-19 13:01:12 +0000

    better logic for checking when user clicks outside of input box

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import cardsData from "./../../python/cards.json";
 import mapsData from "./../../python/maps.json";
 import Image from "next/image";
@@ -15,6 +15,21 @@ type Props = {
 const MapCardSearchBar = ({ placeholder }: Props) => {
   const [search, setSearch] = useState("");
   const [isFocused, setIsFocused] = useState<boolean>(false);
+  const thisRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (thisRef.current && !thisRef.current.contains(event.target as Node)) {
+        setIsFocused(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [thisRef]);
 
   const cards = Object.values(cardsData);
   const maps = Object.values(mapsData);
@@ -27,15 +42,9 @@ const MapCardSearchBar = ({ placeholder }: Props) => {
 
   const searchResults: SearchResult[] = [];
 
-  let filteredCards = cards.filter((card) => {
-    return card.alias.includes(cleanSearch);
-  });
+  cards.forEach((card) => {
+    if (!card.alias.includes(cleanSearch)) return;
 
-  let filteredMaps = maps.filter((map) => {
-    return map.alias.includes(cleanSearch);
-  });
-
-  filteredCards.forEach((card) => {
     searchResults.push({
       id: card.id,
       name: card.name,
@@ -43,7 +52,9 @@ const MapCardSearchBar = ({ placeholder }: Props) => {
     });
   });
 
-  filteredMaps.forEach((map) => {
+  maps.forEach((map) => {
+    if (!map.alias.includes(cleanSearch)) return;
+
     searchResults.push({
       id: map.id,
       name: map.name,
@@ -53,17 +64,12 @@ const MapCardSearchBar = ({ placeholder }: Props) => {
 
   return (
     <div
+      ref={thisRef}
       className={
         "transition-all duration-200 ease-in-out " +
         (isFocused ? "w-72" : "w-60")
       }
     >
-      <label
-        htmlFor="div-map-search"
-        className="sr-only mb-2 text-sm font-medium text-gray-900"
-      >
-        Search
-      </label>
       <div className="relative">
         <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
           <svg
@@ -85,24 +91,29 @@ const MapCardSearchBar = ({ placeholder }: Props) => {
         <input
           type="search"
           id="div-map-search"
+          ref={inputRef}
           className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-10 text-sm text-gray-900 placeholder:text-center  focus:border-blue-500 focus:ring-blue-500"
           placeholder={placeholder}
           autoComplete="off"
           onChange={(e) => setSearch(e.target.value)}
           onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
         />
       </div>
-      {searchResults.length < 10 && searchResults.length > 0 && isFocused ? (
+      {searchResults.length > 0 && searchResults.length < 50 && isFocused ? (
         <div
           id="dropdown-menu"
-          className="relative right-0 rounded-md bg-white p-1 shadow-lg ring-1 ring-black ring-opacity-5"
+          className="relative right-0 max-h-44 overflow-auto rounded-md bg-white p-1 shadow-lg ring-1 ring-black ring-opacity-5"
         >
           {searchResults.map((result) => (
             <div
               key={result.id}
               className="flex h-8 cursor-pointer rounded-md hover:bg-gray-300"
-              onClick={() => console.log("Clicked " + result.name)}
+              onClick={() => {
+                if (inputRef.current != null) {
+                  inputRef.current.value = result.name;
+                  setIsFocused(false);
+                }
+              }}
             >
               <Image
                 src={getIconForType(result.type)}
@@ -132,6 +143,7 @@ function getIconForType(type: SearchResultType) {
       return UniqueMapIcon;
   }
 }
+
 type SearchResult = {
   id: string;
   name: string;
